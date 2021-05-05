e0b4edca07a10f43dbe46c10e7c72407947430c7
doowon13 <endnjs2007@gmail.com>
2021-05-05 11:40:07 +0900

    에러 로그 찍어보기

--- app/src/main/java/com/pluto/kotlinbasic01_doowon/MainActivity.kt
+++ app/src/main/java/com/pluto/kotlinbasic01_doowon/MainActivity.kt
@@ -2,6 +2,7 @@ package com.pluto.kotlinbasic01_doowon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,7 +10,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        clickBtn.setOnClickListener {
 
+            Log.d("메인화면", "클릭용 버튼 눌림")
+            Log.e("메인화면", "에러 관련 로그")
 
+        }
     }
 }
