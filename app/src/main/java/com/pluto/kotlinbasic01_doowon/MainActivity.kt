d2c7eec22d7e9f056bc4ee69107269c995978158
doowon13 <endnjs2007@gmail.com>
2021-05-06 11:27:49 +0900

    Toast 띄워보기

--- app/src/main/java/com/pluto/kotlinbasic01_doowon/MainActivity.kt
+++ app/src/main/java/com/pluto/kotlinbasic01_doowon/MainActivity.kt
@@ -3,6 +3,7 @@ package com.pluto.kotlinbasic01_doowon
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,10 +12,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         clickBtn.setOnClickListener {
+//            코드에 메모 남기기 - 주석처리
+//            {  } 안에있는 코드만, 버튼이 눌렸을 때 실행된다.
 
-            Log.d("메인화면", "클릭용 버튼 눌림")
-            Log.e("메인화면", "에러 관련 로그")
+            Log.d("메인화면 로그", "클릭용 버튼이 눌림")
+            Log.e("메인화면 로그", "에러 관련 로그")
 
         }
+
+        smallBtn.setOnClickListener {
+
+            Toast.makeText(this, "작은 버튼이 눌림", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
