05676df708b9f2e595c3f922220b4b900d355475
doowon13 <endnjs2007@gmail.com>
2021-05-06 13:49:28 +0900

    if else 구문 나이로 판별하기 / 주석처리

--- app/src/main/java/com/pluto/kotlinbasic01_doowon/MainActivity.kt
+++ app/src/main/java/com/pluto/kotlinbasic01_doowon/MainActivity.kt
@@ -23,6 +23,30 @@ class MainActivity : AppCompatActivity() {
 //        clickBtn의 중괄호 안에 있지 않아서 Logcat에 안뜨나?
 //          중괄호 안에 넣어서 실행하니 뜨는것을 확인.
 
+//        문법 연습 (조건문)
+//        나이를 확인 > 성인 / 성인이 아님
+//
+//        val userAge = 14
+//
+//        if (userAge >= 20) {
+//            Log.d("성인판별", "성인이 맞습니다.")
+//        }
+//
+//        else if (userAge >= 17) {
+//            Log.d("성인판별", "고등학생입니다.")
+//        }
+//
+//        else if (userAge >= 14) {
+//            Log.d("성인판별", "중학생입니다.")
+//        }
+//        else {
+//            Log.d("성인판별", "성인이 아닙니다.")
+//        }
+
+
+
+
+
         clickBtn.setOnClickListener {
 //            코드에 메모 남기기 - 주석처리
 //            {  } 안에있는 코드만, 버튼이 눌렸을 때 실행된다.
